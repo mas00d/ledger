d5e7936c611742471a5186f17b97a8341adfa560
masood <masood@a.b>
2020-11-19 10:40:54 +0330

    پروژه maven ای

---
+++ src/main/java/masood/ledger/Main.java
@@ -0,0 +1,13 @@
+package masood.ledger;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
